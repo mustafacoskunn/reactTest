20fdc40f74134d880241edf24107b6397c045bd2
mustafacoskunn <l1621012067@stud.sdu.edu.tr>
2019-10-02 22:47:16 +0300

    first commit

---
+++ src/CategoryList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CategoryList extends Component {
+    render() {
+        return (
+            <div>
+                <h3>CategoryList Component</h3>
+            </div>
+        )
+    }
+}

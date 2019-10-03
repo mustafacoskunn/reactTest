7ae9f5b84dd20909b201219b0ecb91ced5f0ae40
mustafacoskunn <l1621012067@stud.sdu.edu.tr>
2019-10-03 16:40:02 +0300

    eventler

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
- /* constructor(props) {
+  /* constructor(props) {
     super(props);
     this.state = {
       categories: [
@@ -10,26 +10,32 @@ export default class CategoryList extends Component {
         { categoriId: 2, categoryName: "Condimetals" }
       ]
     };
-  }*///olmasada oluyor props=compotentler arası data taşıma //state:kendi componentin datası
+  }*/ //olmasada oluyor props=compotentler arası data taşıma //state:kendi componentin datası
   state = {
     categories: [
       { categoriId: 1, categoryName: "Bavengers" },
       { categoriId: 2, categoryName: "Condimetals" }
-    ]
+    ],
+    currentCategory: ""
+  };
+  changeCategory = category => {
+    this.setState({ currentCategory: category.categoryName });
   };
   render() {
-  
-
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
           {this.state.categories.map(category => (
-            <ListGroupItem key={category.categoriId}>
+            <ListGroupItem
+              onClick={() => this.changeCategory(category)}
+              key={category.categoriId}
+            >
               {category.categoryName}
             </ListGroupItem>
           ))}
         </ListGroup>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     );
   }
